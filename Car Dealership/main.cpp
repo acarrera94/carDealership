56bc72bdab94b10227589d89fcce31a4f24d5b7f
Andre Carrera <acarrera94@gmail.com>
2017-07-22 17:26:20 -0600

    init commit

---
+++ Car Dealership/main.cpp
@@ -0,0 +1,771 @@
+/*
+ 
+ TEST CASES: 
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 3
+ Enter the name of the new car: jeep
+ Enter the color of the new car: blue
+ Enter the price of the new car: 100
+ Car bought.
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 3
+ Enter the name of the new car: toyota
+ Enter the color of the new car: green
+ Enter the price of the new car: 200
+ Car bought.
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 2
+ 
+ Current Balance: $9700.00
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 6
+ What is the name of the file to open? cars1.txt
+ File Opened.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 1
+ Name: jeep
+ Color: blue
+ Price: $100
+ 
+ Name: toyota
+ Color: green
+ Price: $200
+ 
+ Name: Jalopy
+ Color: Blue
+ Price: $3402.99
+ 
+ Name: Rustbucket
+ Color: Brown
+ Price: $44.99
+ 
+ Name: Lemon
+ Color: Yellow
+ Price: $4226.99
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 2
+ 
+ Current Balance: $10229.23
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 7
+ What would you like to name the file? car3.txt
+ File Saved.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 6
+ What is the name of the file to open? car3.txt
+ File Opened.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 1
+ Name: jeep
+ Color: blue
+ Price: $100
+ 
+ Name: toyota
+ Color: green
+ Price: $200
+ 
+ Name: Jalopy
+ Color: Blue
+ Price: $3402.99
+ 
+ Name: Rustbucket
+ Color: Brown
+ Price: $44.99
+ 
+ Name: Lemon
+ Color: Yellow
+ Price: $4226.99
+ 
+ Name: jeep
+ Color: blue
+ Price: $100
+ 
+ Name: toyota
+ Color: green
+ Price: $200
+ 
+ Name: Jalopy
+ Color: Blue
+ Price: $3402.99
+ 
+ Name: Rustbucket
+ Color: Brown
+ Price: $44.99
+ 
+ Name: Lemon
+ Color: Yellow
+ Price: $4226.99
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 8
+ Quiting Program
+ 
+ 
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 3
+ Enter the name of the new car: testla
+ Enter the color of the new car: red
+ Enter the price of the new car: 100000.00
+ 
+ Not enough balance to purchase car.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 3
+ Enter the name of the new car: tesla
+ Enter the color of the new car: blue
+ Enter the price of the new car: 1000
+ Car bought.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 5
+ What is the name of the car to paint? tesla
+ What color to paint the car? red
+ Car painted.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 1
+ Name: tesla
+ Color: red
+ Price: $2000
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 4
+ What is the name of the car to sell? tesla
+ Car sold.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 2
+ 
+ Current Balance: $11000.00
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 4
+ What is the name of the car to sell? tesla
+ Car name not found.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 5
+ What is the name of the car to paint? tesla
+ Car name not found.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 8
+ Quiting Program
+ 
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 6
+ What is the name of the file to open? cars2.txt
+ File Opened.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 2
+ 
+ Current Balance: $69715.43
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 1
+ Name: Navy
+ Color: White
+ Price: $6972.15
+ 
+ Name: Kidney
+ Color: Red
+ Price: $3971.15
+ 
+ Name: Refried
+ Color: Brown
+ Price: $9999.99
+ 
+ Name: Garbanzo
+ Color: White
+ Price: $975.21
+ 
+ Name: Black
+ Color: Black
+ Price: $7946.85
+ 
+ Name: Edamame
+ Color: Green
+ Price: $555.55
+ 
+ Name: Lima
+ Color: White
+ Price: $5873.15
+ 
+ Name: Pinto
+ Color: Brown
+ Price: $12369
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 4
+ What is the name of the car to sell? Pinto
+ Car sold.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 7
+ What would you like to name the file? Pinto.txt
+ File Saved.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 6
+ What is the name of the file to open? Pinto.txt
+ File Opened.
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 2
+ 
+ Current Balance: $164168.84
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 1
+ Name: Navy
+ Color: White
+ Price: $6972.15
+ 
+ Name: Kidney
+ Color: Red
+ Price: $3971.15
+ 
+ Name: Refried
+ Color: Brown
+ Price: $9999.99
+ 
+ Name: Garbanzo
+ Color: White
+ Price: $975.21
+ 
+ Name: Black
+ Color: Black
+ Price: $7946.85
+ 
+ Name: Edamame
+ Color: Green
+ Price: $555.55
+ 
+ Name: Lima
+ Color: White
+ Price: $5873.15
+ 
+ Name: Navy
+ Color: White
+ Price: $6972.15
+ 
+ Name: Kidney
+ Color: Red
+ Price: $3971.15
+ 
+ Name: Refried
+ Color: Brown
+ Price: $9999.99
+ 
+ Name: Garbanzo
+ Color: White
+ Price: $975.21
+ 
+ Name: Black
+ Color: Black
+ Price: $7946.85
+ 
+ Name: Edamame
+ Color: Green
+ Price: $555.55
+ 
+ Name: Lima
+ Color: White
+ Price: $5873.15
+ 
+ 1 - Show Current Inventory
+ 2 - Show Current Balance
+ 3 - Buy a Car
+ 4 - Sell a Car
+ 5 - Paint a Car
+ 6 - Load File
+ 7 - Save File
+ 8 - Quit Program
+ 8
+ Quiting Program
+ 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include "Car.h"
+using namespace std;
+
+void printMenu ();//displays the menu
+
+int nameSearch(const double CAST, string name_in, vector<Car*> cars, const int NOTFOUND);//searches for the name of the car within the vector
+
+void displayInv (vector<Car*> cars);//displays the cars in the vector using class
+
+void buyCar (string name_in, string color_in,//adds a car and it's details to the vector and changes the balance
+             double price_in, int location,
+             const double CAST,vector<Car*> &cars,
+             const int NOTFOUND, double & cashBalance );
+
+void sellCar (string name_in,const double CAST,//removes car and details from the vector  and changes the balance.
+              vector<Car*> &cars,const int NOTFOUND,
+              int location,double & cashBalance  );
+
+void paintCar (string name_in, int location,//changes the color of the car using the class
+               const double CAST, vector<Car*> &cars,
+               const int NOTFOUND, string new_color);
+
+void loadFile (string file_name, double & cashBalance,//loads the file
+               vector<Car*> &cars, double fileBalance,
+               string fileCar, string name_in,
+               string fileColor, string color_in,
+               double fileprice, double price_in);
+
+
+void saveFile (string file_name, double cashBalance,//saves the file
+               vector<Car*> &cars);
+
+
+
+int main() {
+    
+    vector <Car*> cars;
+    
+    int selection =0;
+    const int NOTFOUND = -1;
+    const int INVETORY= 1;
+    const int BALANCE =2;
+    const int BUY = 3;
+    const int SELL = 4;
+    const int PAINT = 5;
+    const int LOADFILE = 6;
+    const int SAVEFILE = 7;
+    const int QUIT = 8;
+    const double CAST = 1.0;
+    const double STARTINGBALANCE =10000.00;
+    
+    
+    double cashBalance =STARTINGBALANCE;//start the program with 10000.00
+    
+    string name_in;
+    string color_in;
+    string new_color;
+    string file_name;
+    double price_in = 0;
+    int location = 0;
+    
+    double fileBalance = 0;
+    string fileCar;
+    string fileColor;
+    double fileprice = 0;
+    
+    while (true) {
+        
+        printMenu();
+        
+        cin >> selection;
+        if (selection == INVETORY ) {
+            displayInv(cars);//shows current invetory
+        }
+        else if (selection == BALANCE ) {
+            cout << endl <<"Current Balance: $" << fixed << setprecision(2) << cashBalance << endl << endl;
+        }
+        else if (selection == BUY ) {
+            buyCar ( name_in,  color_in,//adds a car to the vector and subtracts from balance
+                     price_in,  location,
+                      CAST, cars,
+                      NOTFOUND,cashBalance );
+        }
+        else if (selection == SELL ) {
+            sellCar (name_in,CAST,//removes a car from the vector and adds to balance
+                          cars,NOTFOUND,
+                           location,cashBalance  );
+        }
+        else if (selection == PAINT ) {
+            paintCar ( name_in,  location,//chanes the color and changes the price using the class
+                       CAST, cars,
+                      NOTFOUND,  new_color);
+            
+        }
+        else if (selection == LOADFILE ) {
+            loadFile ( file_name,  cashBalance,//loads the file
+                           cars,  fileBalance,
+                            fileCar,  name_in,
+                            fileColor,  color_in,
+                            fileprice,  price_in);
+        }
+        else if (selection == SAVEFILE ) {
+            
+            saveFile ( file_name,  cashBalance,//saves the file
+                      cars);
+            
+        }
+        else if (selection == QUIT ) {
+            cout << "Quiting Program" << endl;
+            break;
+        }
+    }
+    return 0;
+}
+
+
+void printMenu (){
+    cout << "1 - Show Current Inventory" << endl;
+    cout << "2 - Show Current Balance" << endl;
+    cout << "3 - Buy a Car" << endl;
+    cout << "4 - Sell a Car" << endl;
+    cout << "5 - Paint a Car" << endl;
+    cout << "6 - Load File" << endl;
+    cout << "7 - Save File" << endl;
+    cout << "8 - Quit Program" << endl;
+    
+}
+int nameSearch(const double CAST, string name_in, vector<Car*> cars, const int NOTFOUND){
+    int index = NOTFOUND;//starts out as not found.
+    for (long whichCar = 0; whichCar < cars.size(); whichCar++) {
+        if (cars [whichCar]->getName() ==name_in){
+            index = whichCar*CAST;
+            break;
+        }
+    }
+    return index;
+}
+
+void displayInv (vector<Car*> cars)
+{
+    for (long whichCar =0; whichCar < cars.size(); whichCar++) {
+        cout << cars[whichCar]->toString() << endl;
+    }
+}
+
+void buyCar (string name_in, string color_in,
+             double price_in, int location,
+             const double CAST,vector<Car*> &cars,
+             const int NOTFOUND, double & cashBalance )
+{
+    cout << "Enter the name of the new car: ";
+    cin >> name_in;
+    cout << "Enter the color of the new car: ";
+    cin >> color_in;
+    cout << "Enter the price of the new car: ";
+    cin >> price_in;
+    
+    location = nameSearch(CAST, name_in, cars, NOTFOUND);//checks to see if name is already there
+    
+    if (price_in > cashBalance) {//if there's not enough balance
+        cout << endl << "Not enough balance to purchase car." << endl << endl;
+    }
+    else if (location > NOTFOUND){//if the name is already there
+        cout << endl << "Car is already in the inventory." << endl << endl;
+    }
+    else if (price_in <= cashBalance && location== NOTFOUND){//if there is enough balance and car is not already bought
+        cashBalance -= price_in;//price is subtracted from the balance.
+        
+        Car *thisCar = new Car(name_in, color_in, price_in);//puts it on the heap.
+        cars.push_back(thisCar);//adds a car to the vector
+        cout << "Car bought." << endl << endl;
+    }
+}
+
+void sellCar (string name_in,const double CAST,
+              vector<Car*> &cars,const int NOTFOUND,
+              int location,double & cashBalance  )
+{
+    cout << "What is the name of the car to sell? ";
+    
+    cin >> name_in;
+    
+    location = nameSearch(CAST, name_in, cars, NOTFOUND);//checks for the name of the car
+    
+    if (location == NOTFOUND) {//if the name is not found
+        cout << "Car name not found." << endl << endl;
+    }
+    else if (location != NOTFOUND){//if the name is found
+        cashBalance += cars[location]->getPrice();//then put back the money
+        cars.erase(cars.begin()+location);//then erase that name
+        cout << "Car sold." << endl << endl;
+    }
+}
+
+void paintCar (string name_in, int location,
+               const double CAST, vector<Car*> &cars,
+               const int NOTFOUND, string new_color)
+{
+    cout << "What is the name of the car to paint? ";
+    
+    cin >> name_in;
+    
+    location =nameSearch(CAST, name_in, cars, NOTFOUND);//checks for the name of the car
+    
+    if (location == NOTFOUND) {//if the name is not found
+        cout << "Car name not found." << endl << endl;
+    }
+    else if (location != NOTFOUND){//if the name is found
+        cout << "What color to paint the car? ";
+        cin >> new_color;
+        
+        cars[location]->paint(new_color);//paint the car
+        
+        cout << "Car painted." << endl << endl;
+    }
+}
+
+void loadFile (string file_name, double & cashBalance,//saves the file
+               vector<Car*> &cars, double fileBalance,
+               string fileCar, string name_in,
+               string fileColor, string color_in,
+               double fileprice, double price_in)
+{
+    cout << "What is the name of the file to open? ";
+    cin >> file_name;
+    ifstream in_file;
+    in_file.open(file_name);
+    
+    
+    
+    in_file >> fileBalance;
+    
+    cashBalance += fileBalance;//adds it to currents balance
+    
+    while (true) {//while there is still input.
+        in_file >> fileCar >> fileColor >> fileprice;//put this in a loop.
+        if (in_file.fail()) {//if there is no more input
+            break;
+        }
+        //put into the vector.
+        name_in = fileCar;
+        color_in = fileColor;
+        price_in = fileprice;
+        Car *thisCar = new Car(name_in, color_in, price_in);//puts it on the heap.
+        cars.push_back(thisCar);//adds a car to the vector
+        
+    }
+    
+    
+    in_file.close();
+    
+    cout << "File Opened." << endl << endl;
+    
+}
+
+void saveFile (string file_name, double cashBalance,
+               vector<Car*> &cars)
+{
+    cout << "What would you like to name the file? ";
+    cin >> file_name;
+    
+    ofstream out_file;
+    out_file.open (file_name);
+    
+    out_file << fixed << setprecision(2) << cashBalance << endl;
+    
+    //for loop for each car.
+    for (long whichCar = 0; whichCar < cars.size(); whichCar++) {
+        out_file << cars[whichCar]->getName() <<  " ";
+        out_file << cars[whichCar]->getColor() <<  " ";
+        out_file << fixed << setprecision(2) << cars[whichCar]->getPrice() <<  endl;
+        
+    }
+    
+    
+    
+    out_file.close();
+    
+    cout << "File Saved. " << endl << endl;
+}
